176ddd5418a6be5f616afa67d5b987474adfb2ac
shannonfoy <53524440+shannonfoy@users.noreply.github.com>
2019-07-31 13:58:03 +0100

    Add files via upload

---
+++ memes/script.js
@@ -0,0 +1,20 @@
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+document.getElementById("myButton").onclick = function () {
+	"use strict";
+        location.href = "act2path2.html";
+    };
